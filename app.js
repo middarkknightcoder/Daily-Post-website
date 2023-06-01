c5f839d79e515f14f2ee8549ab18b5d2e186c4a9
Ronak <ronak.kantariya2004@gmail.com>
2023-06-01 23:00:45 +0530

    Add some imoportent files and database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash"); // This is used for the make same string for ex : Allastring converting into lowercase ,capitalize or etc
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,13 +19,36 @@ app.use(express.static("public")); // Here make public folder as a static folder
 
 // Here create global variable
 
-let Posts = [];
+// let Posts = [];
+
+// Here connect mongoose with mongodb database
+
+mongoose.connect("mongodb+srv://host-Ronak:MongodbAtlas2004@ronakdb.gkb7bps.mongodb.net/postsDB", { useNewUrlParser: true });
+
+const postSchema = {
+
+  Title: String,
+  Content: String
+
+};
+
+const Post = mongoose.model("Post", postSchema); // This is model / collection of the postsDB 
+
 
 //Here Home ejs templet route into the root route
 
 app.get("/", function (req, res) {
 
-  res.render("home", { startingContent: homeStartingContent, postsArr: Posts });
+  Post.find()
+    .then(function (foundPosts) {
+
+      res.render("home", { startingContent: homeStartingContent, postsArr: foundPosts });
+
+    })
+    .catch(function (err) {
+
+      console.log(err);
+    })
 
 });
 
@@ -52,36 +76,70 @@ app.get("/compose", function (req, res) {
 
 // Here set a cosutom route path using the express route parameter
 
-app.get("/post/:postName", function (req, res) {
+app.get("/post/:postId", function (req, res) {
 
-  const customPost = _.lowerCase(req.params.postName);
 
-  Posts.forEach(function (post) {
+  // You can also this problem solve using the customPost name 
 
-    let postT = _.lowerCase(post.Title);
+  // const customPost = _.lowerCase(req.params.postName);
 
-    if (postT === customPost) {
+  // Post.find()
 
-      res.render("post", { postS: post });
+  //   .then(function (posts) {
 
-      res.redirect("/post/" + customPost);
-    }
+  //     posts.forEach(function (post) {
+  //       let postT = _.lowerCase(post.Title);
 
-  });
+  //       if (postT === customPost) {
 
+  //         res.render("post", { postS: post });
+
+  //         res.redirect("/post/" + customPost);
+  //       }
+  //     })
+
+  //   })
+  //   .catch(function (err) {
+
+  //     console.log(err);
+  //   })
+
+  let requestId = req.params.postId;
+
+  Post.findOne({ _id: requestId })
+    .then(function (foundPost) {
+
+      res.render("post", { postS: foundPost });
+
+      res.redirect("/post/" + requestId);
+    })
+    .catch(function (err) {
+
+      console.log(err);
+    })
 });
 
 
 app.post("/compose", function (req, res) {
 
-  const Post = {
+  const post = {
 
     Title: req.body.postTitle,
     Content: req.body.postBody
 
   };
 
-  Posts.push(Post);
+  // Posts.push(Post);
+
+  Post.insertMany(post)
+    .then(function () {
+
+      console.log("Successfully added data");
+    })
+    .catch(function (err) {
+
+      console.log(err);
+    })
 
   res.redirect("/");
 
